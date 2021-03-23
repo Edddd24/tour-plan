34ee86ead53c7c1b5a1e10d77265462e49dfd386
Edddd24 <titan.elder2015@gmail.com>
2021-03-23 22:11:10 +0200

    create: added java

--- js/main.js
+++ js/main.js
@@ -18,12 +18,9 @@ $(document).ready(function () {
     },
   });
 
-  var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener("click", function () {
-    console.log("Клик по кнопке меню");
-    document
-      .querySelector(".navbar-buttom")
-      .classList.toggle("navbar-buttom--visible");
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-buttom").toggleClass("navbar-buttom--visible");
   });
 
   var modalButton = $("[data-toggle=modal]");
@@ -32,10 +29,9 @@ $(document).ready(function () {
   closeModalButton.on("click", closeModal);
 
   function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass("modal__overlay--visible");
-    modalDialog.addClass("modal__dialog--visible");
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
   }
   function closeModal(event) {
     event.preventDefault();
@@ -44,4 +40,47 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  $(".modal__form").validate({
+    messages: {
+      name: "Please specify your name",
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "We need your phone number to contact you",
+      },
+    },
+  });
+  $(".forma").validate({
+    messages: {
+      name: "Please specify your name",
+      email: {
+        required: "We need your email address to contact you",
+      },
+      phone: {
+        required: "We need your phone number to contact you",
+      },
+    },
+  });
+  $(".formo").validate({
+    messages: {
+      name: "Please specify your name",
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "We need your phone number to contact you",
+      },
+    },
+  });
+  $(".forme").validate({
+    messages: {
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+    },
+  });
 });
